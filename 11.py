21162fc622d5f752b7883fc9e67a152585d46e4f
atlifryer <atli1902@gmail.com>
2024-12-11 21:25:52 +0000

    hættur að nota string

--- 11.py
+++ 11.py
@@ -1,46 +1,37 @@
 import sys
+from math import log10
 
-content = sys.stdin.read().split(" ")
-content[-1] = content[-1].rstrip("\n")
+content = [872027, 227, 18, 9760, 0, 4, 67716, 9245696]
 
-memo = {}
+memo: dict = {}
 
-def count_stones(num_str, steps):
-	if steps == 0:
-		return 1
 
-	key = (num_str, steps)
-	if key in memo:
-		return memo[key]
+def count_stones(num, steps):
+    if steps == 0:
+        return 1
 
-	integer = int(num_str)
-	digits = len(num_str)
+    key = (num, steps)
+    if key in memo:
+        return memo[key]
 
-	if integer == 0:
-		result = count_stones("1", steps - 1)
-	elif digits % 2 == 0:
-		mid = digits // 2
-		left = str(int(num_str[:mid]))
-		right = str(int(num_str[mid:]))
-		result = count_stones(left, steps - 1) + count_stones(right, steps - 1)
-	else:
-		new_val = str(integer * 2024)
-		result = count_stones(new_val, steps - 1)
-	
-	memo[key] = result
-	return result
+    if num == 0:
+        result = count_stones(1, steps - 1)
+    elif int(log10(num)) % 2 == 1:
+        digits = int(log10(num)) + 1
+        mid = 10 ** (digits // 2)
+        left = num // mid
+        right = num % mid
+        result = count_stones(left, steps - 1) + count_stones(right, steps - 1)
+    else:
+        result = count_stones(num * 2024, steps - 1)
+
+    memo[key] = result
+    return result
+
 
 print("part 1:")
-part1 = sum(count_stones(str(n), 25) for n in content)
+part1 = sum(count_stones(n, 25) for n in content)
 print(part1)
 print("\npart 2:")
-part2 = sum(count_stones(str(n), 75) for n in content)
+part2 = sum(count_stones(n, 75) for n in content)
 print(part2)
-
-
-
-
-
-
-
-
